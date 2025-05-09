e2f27059d9b259ef9aa7ebdac10b734b4b0c41fc
Desperatiz <202360508+Desperatiz@users.noreply.github.com>
2025-05-09 11:01:55 -0600

    Created ParentNode class and implemented basic tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,3 +30,23 @@ class LeafNode(HTMLNode):
         if self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Parent nodes must have a tag")
+        if self.children is None:
+            raise ValueError("Parent nodes must have a children")
+        
+        html = F"<{self.tag}>"
+        
+        for child in self.children:
+            html += child.to_html()
+
+        html += f"</{self.tag}>"
+
+        return html
